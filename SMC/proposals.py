ab8226026e301078564127a06dfa70993726af6b
sean-parry <seancameronparry@gmail.com>
2024-12-11 12:44:27 +0000

    added defensive sampling scheme to proposals

--- SMC/proposals.py
+++ SMC/proposals.py
@@ -44,7 +44,7 @@ class Random_Walk(Proposal):
             p = prob_utils.multivariate_normal_p(new_s, old_s, self.covar_mat)
             self.probs.append(p)
 
-    def update(self, samples : list[list[float]]):
+    def update(self, samples : list[float]):
         """
         updates the object with the new samples and runs the random
         walk on the new samples
@@ -60,6 +60,63 @@ class Random_Walk(Proposal):
         self._calc_p()
         self.new_samples = np.array(self.new_samples)
         self.probs = np.array(self.probs)
+
+
+class Defensive_Sampling(Proposal):
+    def __init__(self, step_probs : list[float] = [0.9, 0.1], 
+                 step_sizes : list[float] = [0.1, 1.0], 
+                 old_samples : list[list[float]] = None):
+        super().__init__(old_samples)
+
+        if sum(step_probs) == 1:
+            self.step_probs = step_probs
+        else:
+            print('Warning step_probs should sum to one, step_probs array given has been normalised')
+            self.step_probs = step_probs / sum(step_probs)
+
+        self.cdf_probs = [0.0]
+        for step_prob in step_probs:
+            self.cdf_probs.append(self.cdf_probs[-1] + step_prob)
+        self.step_sizes = step_sizes
+        if old_samples is not None:
+            self.update(old_samples)
+        return
+    
+    def _walk(self):
+        # could have probably just used np.choice and made an array of step sizes 
+        # then just used some basic matrix maths, change that i guess if its making 
+        # it slow
+        unif_probs = np.random.rand(self.old_samples.shape[0])
+        noise_arr = np.random.standard_normal(self.old_samples.shape)
+        self.new_samples = np.zeros(self.old_samples.shape)
+        for i, (unif_prob, unscaled_noise, old_sample) in enumerate(zip(unif_probs, noise_arr, self.old_samples)):
+            for cdf_prob, step_size in zip(self.cdf_probs[1:], self.step_sizes):
+                if unif_prob<cdf_prob:
+                    print(f'old sampls: {old_sample}, step size {step_size} , unscaled noise {unscaled_noise}')
+                    self.new_samples[i] = old_sample + unscaled_noise * step_size
+                    break
+    
+    def _calc_p(self):
+        self.probs = np.zeros(self.old_samples.shape[0])
+        for i, (old_sample, new_sample) in enumerate(zip(self.old_samples, self.new_samples)):
+            for w, step in zip(self.step_probs, self.step_sizes):
+                self.probs[i] += w * prob_utils.multivariate_normal_p(new_sample, old_sample, self.identity*step)
+        self.probs = np.array(self.probs)
+    
+
+    def update(self, samples : list[list[float]]):
+        """
+        updates the object with the new samples and runs the random
+        walk on the new samples
+        """
+        samples = np.array(samples)
+        self.identity = np.eye(samples.shape[1])
+        self.old_samples = samples
+        self._walk()
+        self._calc_p()
+        self.new_samples = np.array(self.new_samples)
+        self.probs = np.array(self.probs)
+    
 
 class NUTS(Proposal):
     def __init__(self, log_target = None, mass_vec: list[float] = None,
@@ -101,6 +158,11 @@ class NUTS(Proposal):
 
 
 def main():
+    df = Defensive_Sampling()
+    df.update(np.random.rand(10,2))
+    print(df.new_samples)
+    print(df.probs)
+    return
     rw = Random_Walk()
     rw.update(np.random.rand(10,2))
     rw.update(np.random.rand(10,2))
@@ -112,7 +174,7 @@ def main():
     step_sizes = [0.2,0.2]
     nuts = NUTS(target, [1, 1])
     nuts.update(old_samples)
-
+    return
 
 
 if __name__ == '__main__':
